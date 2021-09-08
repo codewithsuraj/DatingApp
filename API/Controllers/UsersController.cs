cabbfb63f7451bbe793080711b7b2318927d2e23
Suraj Mahendrakar <suraj.mahendrakar@primussoft.com>
2021-09-08 20:22:15 +0530

    End of Section 3

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -20,7 +20,6 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {   
-            // Send all users
             return await _context.Users.ToListAsync();
         }
 
